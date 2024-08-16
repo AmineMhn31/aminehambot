138d35089799bc2a291c7c6a29e1959837e7577f
Mohamed Amine M. <amine19962011@gmail.com>
2024-08-16 22:36:13 +0100

    Update stay_alive.py

--- stay_alive.py
+++ stay_alive.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello World!'
+    return 'Bot is alive!'
 
 def run():
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 10000)  # Use PORT from environment or default to 4000
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
