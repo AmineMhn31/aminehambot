1ac372483fe11f77cc8c974345d1f4535d772f81
Mohamed Amine M. <amine19962011@gmail.com>
2024-08-16 22:26:57 +0100

    Update stay_alive.py

--- stay_alive.py
+++ stay_alive.py
@@ -2,19 +2,19 @@ from flask import Flask
 from threading import Thread
 import os
 
-# Initialize the Flask application
+from flask import Flask
+from threading import Thread
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
+    return 'Hello World!'
 
 def run():
-    # Use the environment-specified port if available; otherwise, default to 4000
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
-    # Start the web server in a new thread
     t = Thread(target=run)
     t.start()
