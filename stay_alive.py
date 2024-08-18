2b431681d66ac19fcd54f77ecfaab15841ae26c5
Mohamed Amine M. <amine19962011@gmail.com>
2024-08-18 13:26:50 +0100

    Update stay_alive.py

--- stay_alive.py
+++ stay_alive.py
@@ -9,7 +9,7 @@ def home():
     return 'Bot is alive!'
 
 def run():
-    port = int(os.environ.get('PORT', 4000))  # Use PORT from environment or default to 4000
+    port = int(os.environ.get('PORT', 8080))  # Use PORT from environment or default to 4000
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
