77f673f6c125c474c49f8d501a60d03b266b9963
Mohamed Amine M. <amine19962011@gmail.com>
2024-08-18 13:25:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 import server
 from stay_alive import keep_alive
 # Paste Token Here if you don't wanna put it in an env. variable for some reason
-TOKEN_INSECURE = "6858198900:AAFRKCWioP__zDWsC_JEWrMQnwGFmPGpbUY"
+TOKEN_INSECURE = "7474041486:AAFLRJZacez8OyYCn5bxta_itkiHiTZ07MU"
 
 if os.name == 'posix':
     TOKEN = subprocess.run(["printenv", "HAMSTER_BOT_TOKEN"],
