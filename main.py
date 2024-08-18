a58f200cf73d5cbae6406b5bfbf6a9f79fcac225
Mohamed Amine M. <amine19962011@gmail.com>
2024-08-18 13:09:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,9 @@ import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 import server
-import stay_alive
 from stay_alive import keep_alive
-
-# Start the keep_alive server
-stay_alive.keep_alive()
-
 # Paste Token Here if you don't wanna put it in an env. variable for some reason
-TOKEN_INSECURE = "7474041486:AAFLRJZacez8OyYCn5bxta_itkiHiTZ07MU"
+TOKEN_INSECURE = "6858198900:AAFRKCWioP__zDWsC_JEWrMQnwGFmPGpbUY"
 
 if os.name == 'posix':
     TOKEN = subprocess.run(["printenv", "HAMSTER_BOT_TOKEN"],
@@ -32,18 +27,31 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.CRITICAL)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+# ========================CIPHER==========================================
+async def cipher(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Should make this a Database probably
+    # with open('user_ids','a') as file:
+    #     file.write(f"{update.effective_chat.first_name} : {update.effective_chat.id}\n")
     await context.bot.send_message(chat_id=update.effective_chat.id, text="🐹")
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=
-        "🔢Today's Cipher Code 16/08/2024📅:\n*A 🔘➖*\n*C ➖🔘➖🔘*\n*H 🔘🔘🔘🔘*\n*V 🔘🔘🔘➖*\n✅CLAIM 1000000💰\.",
-        parse_mode='MARKDOWNV2')
 
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=
-        "THE COMMANDES ARE :\n*/bike*\n*/clone*\n*/cube*\n*/train*\n*/merge*\n*/twerk*\n*/all*\nThese will generate 4 keys for their respective games\.",
+        "🔢Today's Cipher Code 18/08/2024📅:\n*T:  ➖*\n*E:  🔘*\n*L:  🔘➖🔘🔘*\n*E:  🔘*\n*G:  ➖➖🔘*\n*R:  🔘➖🔘* \n*A:  🔘➖*\n*M:  ➖➖*\n✅CLAIM 1000000💰\.",
+        parse_mode='MARKDOWNV2')
+
+    # ==================================================================
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Should make this a Database probably
+    # with open('user_ids','a') as file:
+    #     file.write(f"{update.effective_chat.first_name} : {update.effective_chat.id}\n")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="🐹")
+    
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=
+        "THE COMMANDES ARE :\n*/cipher*\n*/start*\n*/bike*\n*/clone*\n*/cube*\n*/train*\n*/all*\nThese will generate 4 keys for their respective games\.",
         parse_mode='MARKDOWNV2')
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
@@ -59,24 +67,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def bike(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await generate_keys(update, context, game_id=1)
-
-async def clone(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await generate_keys(update, context, game_id=2)
-
-async def cube(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await generate_keys(update, context, game_id=3)
-
-async def train(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await generate_keys(update, context, game_id=4)
-
-async def mergeaway(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await generate_keys(update, context, game_id=5)
-
-async def twerkrace(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await generate_keys(update, context, game_id=6)
-
-async def generate_keys(update: Update, context: ContextTypes.DEFAULT_TYPE, game_id: int):
     if EXCLUSIVE and not update.effective_chat.id in AUTHORIZED_USERS:
         return
 
@@ -91,7 +81,79 @@ async def generate_keys(update: Update, context: ContextTypes.DEFAULT_TYPE, game
         parse_mode='MARKDOWNV2')
 
     no_of_keys = int(context.args[0]) if context.args else 4
-    keys = await server.run(chosen_game=game_id, no_of_keys=no_of_keys)
+    keys = await server.run(chosen_game=1, no_of_keys=no_of_keys)
+    generated_keys = [f"`{key}`" for key in keys]
+    formatted_keys = '\n'.join(generated_keys)
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text=f"{formatted_keys}",
+                                   parse_mode='MARKDOWNV2')
+    server.logger.info("Message sent to the client.")
+
+
+async def clone(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if EXCLUSIVE and not update.effective_chat.id in AUTHORIZED_USERS:
+        return
+
+    server.logger.info(f"Generating for client: {update.effective_chat.id}")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="🐹")
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text=f"⌛️Generating⏳\.\.\.",
+                                   parse_mode='MARKDOWNV2')
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"⏱️🙆‍♂ This will only take a moment 🙆‍♂⏱️\.\.\.",
+        parse_mode='MARKDOWNV2')
+
+    no_of_keys = int(context.args[0]) if context.args else 4
+    keys = await server.run(chosen_game=2, no_of_keys=no_of_keys)
+    generated_keys = [f"`{key}`" for key in keys]
+    formatted_keys = '\n'.join(generated_keys)
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text=f"{formatted_keys}",
+                                   parse_mode='MARKDOWNV2')
+    server.logger.info("Message sent to the client.")
+
+
+async def cube(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if EXCLUSIVE and not update.effective_chat.id in AUTHORIZED_USERS:
+        return
+
+    server.logger.info(f"Generating for client: {update.effective_chat.id}")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="🐹")
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text=f"⌛️Generating⏳\.\.\.",
+                                   parse_mode='MARKDOWNV2')
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"⏱️🙆‍♂ This will only take a moment 🙆‍♂⏱️\.\.\.",
+        parse_mode='MARKDOWNV2')
+
+    no_of_keys = int(context.args[0]) if context.args else 4
+    keys = await server.run(chosen_game=3, no_of_keys=no_of_keys)
+    generated_keys = [f"`{key}`" for key in keys]
+    formatted_keys = '\n'.join(generated_keys)
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text=f"{formatted_keys}",
+                                   parse_mode='MARKDOWNV2')
+    server.logger.info("Message sent to the client.")
+
+
+async def train(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if EXCLUSIVE and not update.effective_chat.id in AUTHORIZED_USERS:
+        return
+
+    server.logger.info(f"Generating for client: {update.effective_chat.id}")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="🐹")
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text=f"⌛️Generating⏳\.\.\.",
+                                   parse_mode='MARKDOWNV2')
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"⏱️🙆‍♂ This will only take a moment 🙆‍♂⏱️\.\.\.",
+        parse_mode='MARKDOWNV2')
+
+    no_of_keys = int(context.args[0]) if context.args else 4
+    keys = await server.run(chosen_game=4, no_of_keys=no_of_keys)
     generated_keys = [f"`{key}`" for key in keys]
     formatted_keys = '\n'.join(generated_keys)
     await context.bot.send_message(chat_id=update.effective_chat.id,
@@ -121,7 +183,11 @@ async def all(update: Update, context: ContextTypes.DEFAULT_TYPE):
         f"Number of keys set to {no_of_keys}. Context args was {'not empty' if context.args else 'empty'}"
     )
 
-    for i in range(6):  # Updated to loop through all 6 games
+    # This currently overloads the server with login requests
+    # tasks = [bike(update, context), clone(update, context), cube(update, context), train(update, context)]
+    # await asyncio.gather(*tasks)
+
+    for i in range(4):
         keys = await server.run(chosen_game=i + 1, no_of_keys=no_of_keys)
         generated_keys = [f"`{key}`" for key in keys]
         formatted_keys = '\n'.join(generated_keys)
@@ -138,6 +204,9 @@ if __name__ == '__main__':
     start_handler = CommandHandler('start', start, block=False)
     application.add_handler(start_handler)
 
+    cipher_handler = CommandHandler('cipher', cipher, block=False)
+    application.add_handler(cipher_handler)
+
     bike_handler = CommandHandler('bike', bike, block=False)
     application.add_handler(bike_handler)
 
@@ -149,12 +218,6 @@ if __name__ == '__main__':
 
     train_handler = CommandHandler('train', train, block=False)
     application.add_handler(train_handler)
-
-    mergeaway_handler = CommandHandler('merge', mergeaway, block=False)
-    application.add_handler(mergeaway_handler)
-
-    twerkrace_handler = CommandHandler('twerk', twerkrace, block=False)
-    application.add_handler(twerkrace_handler)
 
     all_handler = CommandHandler('all', all, block=False)
     application.add_handler(all_handler)
