85057d390ab46b9fe5bcc289be850dbe326a49a7
Mohamed Amine M. <amine19962011@gmail.com>
2024-08-23 20:47:10 +0100

    Update server.py

--- server.py
+++ server.py
@@ -58,7 +58,17 @@ GAMES = {
         'name': 'Polysphere',
         'appToken': '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
         'promoId': '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
-    },           
+    }, 
+    8: {
+        'name': 'MowandTrim',
+        'appToken': 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+        'promoId': 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+    },  
+    9: {
+        'name': 'MudRacing',
+        'appToken': '8814a785-97fb-4177-9193-ca4180ff9da8',
+        'promoId': '8814a785-97fb-4177-9193-ca4180ff9da8',
+    },             
 }
 
 BASE_URL = 'https://api.gamepromo.io'
