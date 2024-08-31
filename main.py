08b438d5e9df0550effb79b4834983970d860f6a
Mohamed Amine M. <amine19962011@gmail.com>
2024-08-31 17:38:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import requests
 from stay_alive import keep_alive
 
 # Paste Token Here if you don't wanna put it in an env. variable for some reason
-TOKEN_INSECURE = "7474041486:AAFLRJZacez8OyYCn5bxta_itkiHiTZ07MU"
+TOKEN_INSECURE = "7474041486:AAGRj90HoAdC5IF7wx35gYi2qKi2Z9T_1Rw"
 
 if os.name == 'posix':
     TOKEN = subprocess.run(["printenv", "HAMSTER_BOT_TOKEN"], text=True, capture_output=True).stdout.strip()
