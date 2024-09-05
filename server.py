450ac665dfde869856eb741840710ee5ab6c7b55
Mohamed Amine M. <amine19962011@gmail.com>
2024-09-05 22:24:14 +0100

    Update server.py

--- server.py
+++ server.py
@@ -81,6 +81,11 @@ GAMES = {
         'appToken': 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
         'promoId': 'e68b39d2-4880-4a31-b3aa-0393e7df10c7',
      },
+      12 : {
+        'name': 'Stone Age',
+        'appToken': '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
+        'promoId': '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
+    },
 }
 
 BASE_URL = 'https://api.gamepromo.io'
