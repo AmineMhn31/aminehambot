a8e3a70a6e5969b87ae0053bbaf612fa191bb7e5
Mohamed Amine M. <amine19962011@gmail.com>
2024-09-12 17:45:23 +0100

    Update server.py

--- server.py
+++ server.py
@@ -86,6 +86,11 @@ GAMES = {
         'appToken': '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
         'promoId': '04ebd6de-69b7-43d1-9c4b-04a6ca3305af',
     },
+      13: {
+        'name': 'Bouncemasters',
+        'appToken': 'bc72d3b9-8e91-4884-9c33-f72482f0db37',
+        'promoId': 'bc72d3b9-8e91-4884-9c33-f72482f0db37',
+    },
 }
 
 BASE_URL = 'https://api.gamepromo.io'
